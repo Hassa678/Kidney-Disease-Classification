0b446d4af8ca9c9b3187242b7ebee7c2960522b2
unknown <you@example.com>
2024-08-22 10:36:09 +0100

    aws esc config

--- application.py
+++ application.py
@@ -28,9 +28,9 @@ from werkzeug.utils import secure_filename
 #from gevent.pywsgi import WSGIServer
 
 # Define a flask app
-app = Flask(__name__)
+application = Flask(__name__)
 
-#app = application
+app = application
 
 # Model saved with Keras model.save()
 MODEL_PATH ='model/model.h5'
